f55deb636dc28e602c6136e631ddce806fee62af
xkm <ckelsel@hotmail.com>
2022-09-12 12:49:12 +0800

    feat: 删除多余代码

--- mybatis/mybatisExample/src/main/resources/schema.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `blog` (
-                        `id` int AUTO_INCREMENT NOT NULL,
-                        `title` varchar(255) DEFAULT NULL,
-                        PRIMARY KEY (`id`)
-);
